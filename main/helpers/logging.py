2fd639301429de59d45c0768844a7c3801e507df
Ian Vermes <ian.vermes@gmail.com>
2019-01-29 23:36:45 +0000

    The logging.cfg takes a default logfilename value that is interpolated.

--- main/helpers/logging.py
+++ main/helpers/logging.py
@@ -9,5 +9,11 @@ import os
 import logging
 import sys
 
-__CONFIG_FILE = "../../logger_setup.cfg"
-__CONFIG_FILE = os.path.relpath(os.path.join(__file__, __CONFIG_FILE))
+
+def _get_relpath_relative_to_this_py(filename):
+    relative = os.path.join(__file__, filename)
+    relative = os.path.relpath(relative)
+    return relative
+
+__CONFIG_FILE = _get_relpath_relative_to_this_py("../../logger_setup.cfg")
+__DEFAULT_LOGFILENAME = _get_relpath_relative_to_this_py("../../../logs/recompose.log")
--- tests/unit/test_logging.py
+++ tests/unit/test_logging.py
@@ -15,6 +15,11 @@ import configparser
 import unittest
 
 
+def tearDownModule():
+    logdir = "./logs"
+    for file in os.listdir(logdir):
+        os.remove(os.path.join(logdir, file))
+
 class Test_Logging_Setup(BaseTestCase):
 
     @classmethod
@@ -29,6 +34,7 @@ class Test_Logging_Setup(BaseTestCase):
 
     def tearDown(self):
         self.logging_builtin.shutdown()
+
 
     def test_has_config_file(self):
         config = self.config_file
@@ -61,73 +67,122 @@ class Test_Logging_Setup(BaseTestCase):
         assertmsg = f"Parsing {self.config_file} failed. Reason: {errmsg}"
         self.assertFalse(errmsg, msg=assertmsg)
 
+    def test_config_has_default_logfilename_option(self):
+        option_name = "logfilename"
+        config = configparser.ConfigParser()
+        kwargs = {"section": config.default_section, "option": option_name}
+
+        config.read(self.config_file)
+
+        # Check option
+        self.assertTrue(config.has_option(**kwargs))
+        # Check value of option
+        option_value = config.get(**kwargs)
+        is_valid_dir = os.path.isdir(os.path.dirname(option_value))
+        self.assertTrue(is_valid_dir,
+                        msg=("Could not find the directory that "
+                             f"{option_value} would reside in."))
+
+    def test_config_filehandler_section_use_default_logfilename_option(self):
+        option_name = "args"
+        section_name = "handler_fileHandler"
+        unformatted_value = "('%(logfilename)s', 'w')"
+        config = configparser.ConfigParser()
+
+        config.read(self.config_file)
+
+        exp_kwargs = {"section": config.default_section, "option": "logfilename"}
+        expected_substring = config.get(**exp_kwargs)
+        res_kwargs = {"section": section_name, "option": option_name}
+        res_value = config.get(**res_kwargs)
+
+        with self.subTest(test="does_not_leave_unformatted"):
+            self.assertNotEqual(res_value, unformatted_value)
+        with self.subTest(test="does_include"):
+            self.assertIn(expected_substring, res_value)
+        with self.subTest(test="does_interpolate"):
+            exp_mapping = {"logfilename": expected_substring}
+            self.assertEqual(unformatted_value % exp_mapping, res_value)
+
+    def test_helper_logging_module_has_its_own_default_logfilename(self):
+        global_default_logfilename = getattr(helpers.logging, "__DEFAULT_LOGFILENAME")
+
+        is_valid_dir = os.path.isdir(os.path.dirname(global_default_logfilename))
+
+        self.assertTrue(is_valid_dir, msg=("Could not find the directory that "
+                                           f"{global_default_logfilename} "
+                                           "would reside in."))
+
 
 class Test_Logging_Runtime_Behaviour(BaseTestCase):
 
-        @classmethod
-        def setUpClass(cls):
-            import logging
-            import logging.config
-            cls.logging_builtin = logging
-            cls.logging_builtin_config = logging.config
-            cls.config_file = getattr(helpers.logging, "__CONFIG_FILE")
+    @classmethod
+    def setUpClass(cls):
+        cls.config_file = getattr(helpers.logging, "__CONFIG_FILE")
 
-        def setUp(self):
-            pass
+    def setUp(self):
+        import logging
+        import logging.config
+        self.logging_builtin = logging
+        self.logging_builtin_config = logging.config
+        self.remove_these = []
 
-        @classmethod
-        def tearDown(cls):
-            # self.log.uninstall()
-            cls.logging_builtin.shutdown()
+    def tearDown(self):
+        self.logging_builtin.shutdown()
+        for file in self.remove_these:
+            if os.path.exists(file):
+                os.remove(file)
 
-        def test_recomposeLogger_correct_destinations(self):
+    def get_log_filename(self, logger):
+        catch_type = self.logging_builtin.FileHandler
+        output = [h.baseFilename for h in logger.handlers
+                      if isinstance(h, catch_type)]
+        filename = output.pop()
+        self.remove_these.append(filename)
+        return filename
 
-            def get_log_filename(self, logger):
-                catch_type = self.logging_builtin.FileHandler
-                output = [h.baseFilename for h in logger.handlers
-                              if isinstance(h, catch_type)]
-                filename = output.pop()
-                return filename
+    def test_recomposeLogger_correct_destinations(self):
 
-            # Expected values
-            debug_msg = "1debug1"
-            info_msg = "2info2"
-            warning_msg = "3warning3"
-            error_msg = "4error4"
-            should_be_in_stderr = [error_msg]
-            should_be_in_logfile = [error_msg, warning_msg, info_msg, debug_msg]
+        # Expected values
+        debug_msg = "1debug1"
+        info_msg = "2info2"
+        warning_msg = "3warning3"
+        error_msg = "4error4"
+        should_be_in_stderr = [error_msg]
+        should_be_in_logfile = [error_msg, warning_msg, info_msg, debug_msg]
 
-            with testfixtures.OutputCapture(separate=True) as output:
-                # Setup logging
-                self.logging_builtin_config.fileConfig(self.config_file,
-                    disable_existing_loggers=True
-                    )
-                logger = self.logging_builtin.getLogger("recomposeLogger")
-                # Do logging
-                logger.debug(debug_msg)
-                logger.info(info_msg)
-                logger.warning(warning_msg)
-                logger.error(error_msg)
-            # Logging postcondition
-            logfile = get_log_filename(self, logger)
-            stderr = output.stderr.getvalue().strip()
-            stdout = output.stdout.getvalue().strip()
+        with testfixtures.OutputCapture(separate=True) as output:
+            # Setup logging
+            self.logging_builtin_config.fileConfig(self.config_file,
+                disable_existing_loggers=True
+                )
+            logger = self.logging_builtin.getLogger("recomposeLogger")
+            # Do logging
+            logger.debug(debug_msg)
+            logger.info(info_msg)
+            logger.warning(warning_msg)
+            logger.error(error_msg)
+        # Logging postcondition
+        logfile = self.get_log_filename(logger)
+        stderr = output.stderr.getvalue().strip()
+        stdout = output.stdout.getvalue().strip()
 
-            # Test for stdout
-            with self.subTest(output="stdout"):
-                self.assertEqual(stdout, "")
+        # Test for stdout
+        with self.subTest(output="stdout"):
+            self.assertEqual(stdout, "")
 
-            # Test for stderr
-            with self.subTest(output="stderr"):
-                self.assertSubstringsInString(should_be_in_stderr, stderr)
+        # Test for stderr
+        with self.subTest(output="stderr"):
+            self.assertSubstringsInString(should_be_in_stderr, stderr)
 
-            # Test for logfile
-            self.logging_builtin.shutdown()
-            with self.subTest(output=os.path.basename(logfile)):
-                with open(logfile) as handle:
-                    lines = handle.readlines()
-                    length = len(lines)
-                    log_lines = "".join(lines).strip()
-                self.assertSubstringsInString(should_be_in_logfile, log_lines,
-                                              msg=f"Looking in {logfile}")
-                self.assertEqual(length, len(should_be_in_logfile))
+        # Test for logfile
+        self.logging_builtin.shutdown()
+        with self.subTest(output=os.path.basename(logfile)):
+            self.assertTrue(os.path.isfile(logfile))
+            with open(logfile) as handle:
+                lines = handle.readlines()
+                length = len(lines)
+                log_lines = "".join(lines).strip()
+            self.assertSubstringsInString(should_be_in_logfile, log_lines,
+                                          msg=f"Looking in {logfile}")
+            self.assertEqual(length, len(should_be_in_logfile))
